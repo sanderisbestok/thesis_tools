2ed5f0fffec4071039880b3ff8e6e2ce2634c0df
Sander Hansen <sanderisbestok@gmail.com>
2021-04-06 15:57:17 +0200

    Finsihed tool, ego_to_json

--- ego_to_json.py
+++ ego_to_json.py
@@ -2,12 +2,10 @@ import os
 import shutil 
 import json
 import scipy.io
+import random
 
-# dit moet alles gaan splitten zodat het daarna omgezet kan worden.
-# 1 situatie er buiten houden voor test?
-
-#zorgen dat ik data kan splitten, en er annotations files bij heb die dan bruikbaar zijn voor alle andere algoritmes!
-
+# ego_to_json.py maakt egohands_data bruikbaar om om te laten zetten naar de verschillende
+# formaten voor de netwerken.
 #drie folders, train, test, val
 
 # |-- train
@@ -104,14 +102,55 @@ def create_annotations(directory, img_dir):
     for dir_name in os.listdir(directory):
         if not (dir_name == "images" or dir_name == "annotations.json"):
             shutil.rmtree(os.path.join(directory, dir_name))
-                             
+
+def split_train_val():    
+    tmp_dir = os.path.join(ROOT_DIR, "tmp")
+    
+    os.makedirs(os.path.join(ROOT_DIR, "train"))
+    train_dir = os.path.join(ROOT_DIR, "train")
+    os.makedirs(os.path.join(train_dir, "images"))
+    
+    os.makedirs(os.path.join(ROOT_DIR, "val"))
+    val_dir = os.path.join(ROOT_DIR, "val")
+    os.makedirs(os.path.join(val_dir, "images"))
+
+    # Opening JSON file
+    with open(os.path.join(tmp_dir, 'annotations.json')) as json_file:
+        data = json.load(json_file)
+
+        # 0.1765 is 15% van 100%
+        val_keys = random.sample(list(data), round(len(data) * 0.1765))
+
+        validation = {k: v for k, v in data.items() if k in val_keys}
+        train = {k: v for k, v in data.items() if k not in val_keys}
+
+    with open(os.path.join(val_dir, SAVE_FILE), 'w') as output_json_file:
+        json.dump(validation, output_json_file)
+
+    with open(os.path.join(train_dir, SAVE_FILE), 'w') as output_json_file:
+        json.dump(train, output_json_file)
+        
+    for key, _ in validation.items():
+        shutil.move(os.path.join(tmp_dir, "images", key), os.path.join(val_dir, "images", key))
+
+    for key, _ in train.items():
+        shutil.move(os.path.join(tmp_dir, "images", key), os.path.join(train_dir, "images"))
+
+    shutil.rmtree(tmp_dir)
+
+    
+
+        
+
+
+
 
 split_test()
 json_test()
 
 json_train_val()
-# split_train_val()
-        
 
-# os.makedirs(os.path.join(ROOT_DIR, "train"))
-# os.makedirs(os.path.join(ROOT_DIR, "val"))
+# shutil.rmtree(os.path.join(ROOT_DIR, "val"))
+# shutil.rmtree(os.path.join(ROOT_DIR, "train"))
+
+split_train_val()
